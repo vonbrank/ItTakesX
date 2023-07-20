fd25e99361c3acb0d68a19c97aca63c30a9a01af
vonbrank <vonbrank@outlook.com>
2023-07-20 17:58:20 +0800

    feat: 添加 HoveringCar 核心组件及其零件

---
+++ Source/ItTakesX/Public/BuildingSystem/Component/VehicleComponentHoveringWheel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildingSystem/VehicleComponentActor.h"
+#include "VehicleComponentHoveringWheel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITTAKESX_API AVehicleComponentHoveringWheel : public AVehicleComponentActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ItTakesX/Public/BuildingSystem/Core/VehicleCoreHoveringCar.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildingSystem/VehicleCoreActor.h"
+#include "VehicleCoreHoveringCar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITTAKESX_API AVehicleCoreHoveringCar : public AVehicleCoreActor
+{
+	GENERATED_BODY()
+	
+};
