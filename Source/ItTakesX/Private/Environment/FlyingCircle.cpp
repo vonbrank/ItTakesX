596ec2819474b3f433a680ea1d444f0a45d65797
vonbrank <vonbrank@outlook.com>
2023-08-15 22:09:34 +0800

    feat: 添加飞行路径导航点

---
+++ Source/ItTakesX/Private/Environment/FlyingCircle.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Environment/FlyingCircle.h"
+
+#include "Character/ItTakesXCharacter.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFlyingCircle::AFlyingCircle()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AreaBox = CreateDefaultSubobject<UBoxComponent>(TEXT("AreaBox"));
+	SetRootComponent(AreaBox);
+
+	FirstPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FirstPlane"));
+	FirstPlane->SetupAttachment(RootComponent);
+
+	SecondPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SecondPlane"));
+	SecondPlane->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AFlyingCircle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AreaBox->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBoxStartOverlap);
+	AreaBox->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnBoxEndOverlap);
+
+	FirstPlaneMaterial = FirstPlane->CreateDynamicMaterialInstance(0);
+	SecondPlaneMaterial = SecondPlane->CreateDynamicMaterialInstance(0);
+	if (FirstPlaneMaterial)
+	{
+		// FirstPlaneMaterial->SetScalarParameterValue(TEXT("ColorBright"), 10);
+		FirstPlaneMaterial->SetVectorParameterValue(FName("BaseColor"), OriginColor);
+	}
+	if (SecondPlaneMaterial)
+	{
+		// SecondPlaneMaterial->SetScalarParameterValue(TEXT("ColorBright"), 10);
+		SecondPlaneMaterial->SetVectorParameterValue(FName("BaseColor"), OriginColor);
+	}
+}
+
+// Called every frame
+void AFlyingCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFlyingCircle::OnBoxStartOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+	auto Character = Cast<AItTakesXCharacter>(OtherActor);
+	if (Character && bCharacterHasCrossed == false)
+	{
+		bCharacterHasCrossed = true;
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("Character first Overlap")));
+
+		if (FirstPlaneMaterial)
+		{
+			// FirstPlaneMaterial->SetScalarParameterValue(TEXT("ColorBright"), 10);
+			FirstPlaneMaterial->SetVectorParameterValue(FName("BaseColor"), CrossColor);
+		}
+		if (SecondPlaneMaterial)
+		{
+			// SecondPlaneMaterial->SetScalarParameterValue(TEXT("ColorBright"), 10);
+			SecondPlaneMaterial->SetVectorParameterValue(FName("BaseColor"), CrossColor);
+		}
+	}
+}
+
+void AFlyingCircle::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
---
+++ Source/ItTakesX/Public/Environment/FlyingCircle.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FlyingCircle.generated.h"
+
+UCLASS()
+class ITTAKESX_API AFlyingCircle : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFlyingCircle();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* FirstPlane;
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* SecondPlane;
+
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* AreaBox;
+
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	class UMaterialInstanceDynamic* FirstPlaneMaterial;
+
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	class UMaterialInstanceDynamic* SecondPlaneMaterial;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	FColor OriginColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	FColor CrossColor;
+
+	UFUNCTION()
+	virtual void OnBoxStartOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+	UFUNCTION()
+	virtual void OnBoxEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+
+	bool bCharacterHasCrossed = false;
+
+
+public:
+};
